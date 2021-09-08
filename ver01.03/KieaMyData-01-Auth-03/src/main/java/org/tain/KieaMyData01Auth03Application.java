75c3abd8a417310d431f96f9a49cd171a4378a3d
grtlinux <grtlinux@gmail.com>
2021-09-08 14:16:03 +0900

    create 1st

---
+++ ver01.03/KieaMyData-01-Auth-03/src/main/java/org/tain/KieaMyData01Auth03Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaMyData01Auth03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaMyData01Auth03Application.class, args);
+	}
+
+}
