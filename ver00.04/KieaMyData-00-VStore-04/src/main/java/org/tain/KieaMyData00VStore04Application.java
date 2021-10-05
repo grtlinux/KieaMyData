abc8651cb62679da62e901ea3bd5637a888a9c9f
grtlinux <grtlinux@gmail.com>
2021-10-05 17:23:37 +0900

    create 1st

---
+++ ver00.04/KieaMyData-00-VStore-04/src/main/java/org/tain/KieaMyData00VStore04Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaMyData00VStore04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaMyData00VStore04Application.class, args);
+	}
+
+}
---
+++ ver01.04/KieaMyData-01-Auth-04/src/main/java/org/tain/KieaMyData01Auth04Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaMyData01Auth04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaMyData01Auth04Application.class, args);
+	}
+
+}
